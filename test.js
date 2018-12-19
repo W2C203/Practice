a90d31287c3686b1b41d98534e5bd4455ef072aa
JoeLucky <347126391@qq.com>
2018-12-19 09:18:59 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,19 @@
+<script>alert(¡®foolish!¡¯)</script>£¡
+<input type="text" value="ÀÅçð°ñ" onclick="javascript:alert('handsome boy')">
+<strong onclick="alert('suck')"> yoxi  </strong>
+<em onclick="suck">em1</em>999
+<em>em2</em>888
+<input type="text" value="ÀÅçð°ñ" onclick="javascript:alert('handsome boy2')">
+<script>alert('ddddd')</script>
+
+
+***
+
+
+<script>alert('handsome body')</script>123123123
+<em>emself222</em>12312312322
+<em onclick="alert('suck22')"  auto="33rr">fdfdf2222</em>123123
+
+
+  [1]: file:///home/joe/myownpractice/data.json        
+  [2]: file:///home/joe/myownpractice/index.html
